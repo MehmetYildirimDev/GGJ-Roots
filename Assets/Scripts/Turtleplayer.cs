03bc625abddb9504969cf4b2d52f25f6182a14f1
Fatma Şahiner <ftmsahiner01@gmail.com>
2023-02-05 01:06:27 +0300

    feat: create Turtleplayer script

--- Assets/Scripts/Turtleplayer.cs
+++ Assets/Scripts/Turtleplayer.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Turtleplayer : MonoBehaviour
 {
+
+    public GameObject hedefOyuncu;
+    public float kovalamaMesafesi;
+    NavMeshAgent turtleNavMesh;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hedefOyuncu= GameObject.Find("FirstPersonController");
+        turtleNavMesh=this.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     float mesafe= Vector3.Distance(this.transform.position, hedefOyuncu.transform.position); 
+        if (mesafe< kovalamaMesafesi) 
+        {
+            turtleNavMesh.SetDestination(hedefOyuncu.transform.position);
+        }
     }
 }
